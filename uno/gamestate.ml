b35a42be83af33bf94705c32c8adda6bfae35c76
Caroline <cec335@cornell.edu>
2020-04-18 19:34:05 -0400

    command and gamestate templates

--- uno/gamestate.ml
+++ uno/gamestate.ml
@@ -1,10 +1,45 @@
 open Yojson.Basic.Util
 
-(** [shuffle t] returns a new game state with the cards in the discard pile
-    shuffled and . this is called by need_shuffle *)
+type t 
+type card_name = string
+type gamer = User | Player
 
-val shuffle : t -> t
+exception UnknownCard of card_name
+exception CardNotInDeck of card_name 
+exception MisMatch of card_name 
+exception Nouno of gamer
 
-(** [need_shuffle t] returns whether the discard pile needs shuffling. this will
-    be called from draw. *)
-val need_shuffle : t -> bool
+
+let from_json j =
+  failwith "Unimplemented"
+
+let last_card_played t =
+  failwith "Unimplemented"
+
+let hand t gamer =
+  failwith "Unimplemented"
+
+let hand_size t gamer =
+  failwith "Unimplemented"
+
+(** [shuffle t] returns a new game state with the cards in the Discard pile
+    shuffled and reinserted into the Draw pile. This is called by need_shuffle *)
+let shuffle t =
+  failwith "Unimplemented"
+
+(** [need_shuffle t] returns whether the discard pile needs shuffling. This will
+    be called from Draw. *)
+let need_shuffle t =
+  failwith "Unimplemented"
+
+let draw t gamer num =
+  failwith "Unimplemented"
+
+let draw t gamer card_name =
+  failwith "Unimplemented"
+
+let uno_defensive t gamer =
+  failwith "Unimplemented"
+
+let uno_offensive t gamer =
+  failwith "Unimplemented"
