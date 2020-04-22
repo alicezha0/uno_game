17476767c5e2d242fb08aba97de8c2e9bea22a7e
Caroline <cec335@cornell.edu>
2020-04-21 22:15:33 -0400

    finished player.ml!

--- uno/player.ml
+++ uno/player.ml
@@ -18,26 +18,25 @@ type t
 
 (** [number_search hand number] is the name of the first card in [hand] with
     number [number], or an empty string if no such card exists in [hand] *)
-let rec number_search hand number =
+let rec number_search t hand number =
   match hand with
   | [] -> ""
-  | h::t -> if h.number = number then h else number_search t number
-(* replace h.number with a way to retrieve its number*)
+  | h::t -> if Gamestate.number_search t Player h = number then h 
+            else t number_search t number
 
 (** [color_search hand color] is the name of the first card in [hand] with
-    color [color], or an empty string if no such card with color [color] exists
-    in [hand] *)
-let rec color_search hand color =
+    color [color], or an empty string if no such card exists in [hand] *)
+let rec color_search t hand color =
   match hand with
   | [] -> ""
-  | h::t -> if h.color = color then h else color_search t color
-(* replace h.color with a way to retrieve its color*)
+  | h::t -> if Gamestate.color_search t Player h = color then h 
+            else t color_search t color
 
 (** [find_playable_card hand card_name] is the first playable card in [hand], 
-    prioritizing color, then number. Returns an empty string if no such exists *)
-let find_playable_card hand card_name =
-  let same_color_card = color_search hand card_name in
-  let same_num_card = number_search hand card_name in
+    prioritizing color, then number. Returns an empty string if no such card exists *)
+let find_playable_card t hand card_name =
+  let same_color_card = color_search t hand (Gamestate.color_search t Player card_name) in
+  let same_num_card = number_search t hand (Gamestate.number_search t Player card_name) in
   match (same_color_card) with
   | "" -> 
     (match (same_num_card) with
@@ -50,7 +49,7 @@ let player_turn t =
   let hand = Gamestate.hand t Player in
   let last_card = Gamestate.last_card_played t in 
   if Gamestate.hand_size t User = 2 then Uno2 else
-    let playable_card = find_playable_card hand last_card in
+    let playable_card = find_playable_card t hand last_card in
     if Gamestate.hand_size t Player = 2 then match playable_card with
       | "" -> Draw
       | _ -> Uno playable_card
